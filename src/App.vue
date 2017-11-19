6d2da5cd3bb7b9c72cf96f27068d3e094ca47145
Masaya Kazama <miyan@t-p.jp>
2017-11-19 15:14:44 +0900

    Init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <hello-world
+      :text="'world'"
+    ></hello-world>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  props: {
+    size: {
+      type: Number,
+      required: false,
+      default: 10
+    }
+  },
+  computed: {
+    halfSize (): number {
+      return this.size / 2
+    }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Hello, {{text | upper}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  components: {
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  },
+  filters: {
+    upper (text: string): string {
+      return text.toUpperCase()
+    }
+  }
+})
+</script>
+
+<style>
+</style>
